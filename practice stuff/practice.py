8be91ab98d8811a505ad2b0a79f040960f411ab6
Chandler Goewert <cmgoewert@aol.com>
2017-09-17 19:38:45 -0400

    Read in file and write its amplitude numbers to a file

--- practice stuff/practice.py
+++ practice stuff/practice.py
@@ -1,1 +1,16 @@
+import wave, struct
+
 print("hello box munchers")
+
+audioFile = wave.open('Snare-1.wav', 'r')
+numberOutput = open("output.csv", "w")
+numberOutput.write("amplitude" + "\n")
+
+length = audioFile.getnframes()
+print(length)
+for i in range(0,length):
+    waveData = audioFile.readframes(1)
+    data = struct.unpack("<h", waveData)
+    numberOutput.write(str(data[0]) + "\n")
+
+numberOutput.close()
