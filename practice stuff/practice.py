bfb4f762dc67681b1b545a36ed5547e54c39158b
Chandler Goewert <cmgoewert@aol.com>
2017-09-25 17:45:24 -0400

    Librosa beginning

--- practice stuff/practice.py
+++ practice stuff/practice.py
@@ -1,6 +1,13 @@
-import wave, struct
+import wave, struct, librosa
 
-print("hello box munchers")
+print("Starting analysis...")
+
+#this part uses librosa
+audio, sampleRate = librosa.load('Snare-1.wav');
+print(audio)
+print(sampleRate)
+
+#this part uses the wav module
 
 audioFile = wave.open('Snare-1.wav', 'r')
 numberOutput = open("output.csv", "w")
@@ -10,7 +17,10 @@ length = audioFile.getnframes()
 print(length)
 for i in range(0,length):
     waveData = audioFile.readframes(1)
+    #print(str(waveData))
+    #numberOutput.write(str(waveData) + "\n")
     data = struct.unpack("<h", waveData)
     numberOutput.write(str(data[0]) + "\n")
+    print(int(data[0]))
 
 numberOutput.close()
