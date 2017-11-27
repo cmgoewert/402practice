2f0efd5a6b991b1a2be9fa6bbc9d54fd491f2162
Chandler Goewert <cmgoewert@aol.com>
2017-11-27 18:29:10 -0500

    update features to be what we want

--- practice stuff/loadAndExtractAudio.py
+++ practice stuff/loadAndExtractAudio.py
@@ -7,25 +7,41 @@ import tensorflow as tf
 from matplotlib.pyplot import specgram
 
 def extract_feature(file_name):
+    print(file_name)
     X, sample_rate = librosa.load(file_name)
+
+    #compute spectral centroid
+    cent = np.mean(librosa.feature.spectral_centroid(y=X, sr=sample_rate).T,axis=0)
+    print("Spectral Centroid:")
+    print(cent)
+
+    #compute spectral contrast
+    S = np.abs(librosa.stft(X))
+    contrast = np.mean(librosa.feature.spectral_contrast(S=S, sr=sample_rate).T,axis=0)
+    print("Spectral contrast:")
+    print(contrast)
+
+    #compute RMS energy time series
+    rmsTimeSeries = np.mean(librosa.feature.rmse(y=X).T,axis=0)
+    print("RMS time series:")
+    print(rmsTimeSeries)
+
     stft = np.abs(librosa.stft(X))
     mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
     chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
     mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
     contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
-    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
-    return mfccs,chroma,mel,contrast,tonnetz
+    return mfccs,chroma,mel,contrast
 
 def parse_audio_files(parent_dir,sub_dirs,file_ext='*.wav'):
-    features, labels = np.empty((0,193)), np.empty(0)
+    features, labels = np.empty((0,187)), np.empty(0)
     for label, sub_dir in enumerate(sub_dirs):
-        print(parent_dir)
-        print(sub_dir);
-        print(os.path.join(parent_dir, sub_dir, file_ext))
         for fn in glob.glob(os.path.join(parent_dir, sub_dir, file_ext)):
-            print("file found")
-            mfccs, chroma, mel, contrast,tonnetz = extract_feature(fn)
-            ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
+            print("file found:")
+            mfccs, chroma, mel, contrast = extract_feature(fn)
+            ext_features = np.hstack([mfccs,chroma,mel,contrast])
+            print(ext_features.size)
+            #print(np.vstack([features,ext_features]))
             features = np.vstack([features,ext_features])
-            labels = np.append(labels, fn.split('-')[1])
+            labels = np.append(labels, fn.split('-')[1].split('.')[0])
     return np.array(features), np.array(labels, dtype = np.int)
--- practice stuff/task.py
+++ practice stuff/task.py
@@ -1,7 +1,7 @@
 from loadAndExtractAudio import parse_audio_files
 
 
-parent_dir = 'train_claps'
+parent_dir = 'sound_data'
 tr_sub_dirs = ['train']
 #ts_sub_dirs = ['fold3']
 tr_features, tr_labels = parse_audio_files(parent_dir,tr_sub_dirs)
