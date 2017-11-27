ae3f017bd95f406be32677192902974ac2984b33
Chandler Goewert <cmgoewert@aol.com>
2017-11-27 18:43:10 -0500

    Successfully geting features out for train and test sets

--- practice stuff/loadAndExtractAudio.py
+++ practice stuff/loadAndExtractAudio.py
@@ -26,22 +26,21 @@ def extract_feature(file_name):
     print("RMS time series:")
     print(rmsTimeSeries)
 
-    stft = np.abs(librosa.stft(X))
-    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
-    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
-    mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
-    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
-    return mfccs,chroma,mel,contrast
+    #compute RMS energy spectrogram
+    S, phase = librosa.magphase(librosa.stft(X))
+    rmsSpectrogram = np.mean(librosa.feature.rmse(S=S).T,axis=0)
+    print("RMS energy spectrogram:")
+    print(rmsSpectrogram)
+
+    return cent,contrast,rmsTimeSeries,rmsSpectrogram
 
 def parse_audio_files(parent_dir,sub_dirs,file_ext='*.wav'):
-    features, labels = np.empty((0,187)), np.empty(0)
+    features, labels = np.empty((0,10)), np.empty(0)
     for label, sub_dir in enumerate(sub_dirs):
         for fn in glob.glob(os.path.join(parent_dir, sub_dir, file_ext)):
             print("file found:")
-            mfccs, chroma, mel, contrast = extract_feature(fn)
-            ext_features = np.hstack([mfccs,chroma,mel,contrast])
-            print(ext_features.size)
-            #print(np.vstack([features,ext_features]))
+            cent, contrast, rmsTimeSeries, rmsSpectrogram = extract_feature(fn)
+            ext_features = np.hstack([cent,contrast,rmsTimeSeries,rmsSpectrogram])
             features = np.vstack([features,ext_features])
             labels = np.append(labels, fn.split('-')[1].split('.')[0])
     return np.array(features), np.array(labels, dtype = np.int)
--- practice stuff/task.py
+++ practice stuff/task.py
@@ -3,7 +3,6 @@ from loadAndExtractAudio import parse_audio_files
 
 parent_dir = 'sound_data'
 tr_sub_dirs = ['train']
-#ts_sub_dirs = ['fold3']
+ts_sub_dirs = ['test']
 tr_features, tr_labels = parse_audio_files(parent_dir,tr_sub_dirs)
-print(tr_features)
-#ts_features, ts_labels = parse_audio_files(parent_dir,ts_sub_dirs)
+ts_features, ts_labels = parse_audio_files(parent_dir,ts_sub_dirs)
